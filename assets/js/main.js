4ab2163d07a0113686f98991eecaf48e1b094148
pawar-adesh <adesh.pawar19@pccoepune.org>
2022-06-13 19:13:07 +0530

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,22 +95,7 @@ window.location.href.split('#')[0]
       })
     }
   
-    /**
-     * Back to top button
-     */
-    let backtotop = select('.back-to-top')
-    if (backtotop) {
-      const toggleBacktotop = () => {
-        if (window.scrollY > 100) {
-          backtotop.classList.add('active')
-        } else {
-          backtotop.classList.remove('active')
-        }
-      }
-      window.addEventListener('load', toggleBacktotop)
-      onscroll(document, toggleBacktotop)
-    }
-  
+    
     /**
      * Mobile nav toggle
      */
@@ -138,21 +123,7 @@ window.location.href.split('#')[0]
       }
     }, true)
   
-    /**
-     * Scroll with ofset on page load with hash links in the url
-     */
-    window.addEventListener('load', () => {
-      if (window.location.hash) {
-        if (select(window.location.hash)) {
-          scrollto(window.location.hash)
-        }
-      }
-    });
-  
     
-    /**
-     * Animation on scroll
-     */
     window.addEventListener('load', () => {
       AOS.init({
         duration: 1000,
